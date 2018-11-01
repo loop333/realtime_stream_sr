e7e84216a7180c2205267e92c8384ac4439cb5ae
loop333 <37012903+loop333@users.noreply.github.com>
2018-11-02 04:12:39 +0500

    Add files via upload

---
+++ run_bbc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/Users/dima/utils/ffmpeg -loglevel error -i hls+http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_vlow/llnw/bbc_world_service.m3u8 -ar 16000 -ac 1 -f wav -acodec pcm_s16le - | bin/julius -C my.jconf | grep "^sentence"
