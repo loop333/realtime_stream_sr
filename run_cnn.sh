c7fc39524459cd260504b5540b89cf0cb45d1f9f
loop333 <37012903+loop333@users.noreply.github.com>
2018-11-02 17:30:16 +0500

    Update run_cnn.sh

--- run_cnn.sh
+++ run_cnn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/Users/dima/utils/ffmpeg -loglevel error -i hls+https://s3.amazonaws.com/cnn-assets/app-assets/cnn-lite/liteaudio.m3u8 -ar 16000 -ac 1 -f wav -acodec pcm_s16le - | bin/julius -C my.jconf | grep "^sentence"
+ffmpeg -loglevel error -i hls+https://s3.amazonaws.com/cnn-assets/app-assets/cnn-lite/liteaudio.m3u8 -ar 16000 -ac 1 -f wav -acodec pcm_s16le - | bin/julius -C my.jconf | grep "^sentence"
